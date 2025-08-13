0d8b6f5406ec241cdfd4cb923e9559fe1cfd9c27
Louie Mendez <lmendezjr024@gmail.com>
2025-08-13 13:14:46 -0400

    basic echo server/client

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"what is s: {s}")
+    conn, addr = s.accept()
+    print(f"what is conn: {conn}")
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
