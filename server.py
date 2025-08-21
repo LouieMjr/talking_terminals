8750841a999077e11492d5ffb97e0b3ab6c09f43
Louie Mendez <lmendezjr024@gmail.com>
2025-08-21 16:00:00 -0400

    opening local connection to listening socket, that makes accept return for main thread to see signal change

--- server.py
+++ server.py
@@ -1,38 +1,51 @@
 import socket
 import msgpack
 import threading
+from queue import Queue
+
+
 
 connections = []
+threads = []
+running = True
 
 def handle_clients(client_conn):
     try:
         while True:
             data = client_conn.recv(1024)
+            if not data:
+                break
+
             message = msgpack.unpackb(data)
 
             if message.lower() in ('q', 'quit'):
                 client_conn.close()
                 print(f'\nClient wants to terminate.\nConnection closed!')
+                print(f'How many clients connected: {len(connections)}')
                 connections.remove(client_conn)
                 print('Removed client from list.')
+                print(f'How many clients connected: {len(connections)}')
                 break
             else:
                 print(f'\nMessage from client says: {message}')
 
-            server_msg_to_client = msgpack.packb(input('\nSend message to Client: '))
-            client_conn.sendall(server_msg_to_client)
-    except KeyboardInterrupt:
-        server_msg_to_client = msgpack.packb('quit')
-        client_conn.sendall(server_msg_to_client)
-        client_conn.close()
-        connections.remove(client_conn)
-        print('\nClient connection removed.')
-        print(len(connections))
+            server_msg = input('\nSend message to Client: ')
+            if server_msg.lower() in ('q', 'quit'):
+                client_conn.close()
+                global running
+                running = False
+
+                temp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                server_address = ('127.0.0.1', 8000)
+                temp_socket.connect(server_address)
+                temp_socket.close()
+
+                break
+
+            client_conn.sendall(msgpack.packb(server_msg))
 
     finally:
-        if len(connections) == 0:
-            return 0
-        return 1
+        print('Client thread finished')
 
 def start_tcp_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -42,26 +55,34 @@ def start_tcp_server():
     server_socket.listen(5)
     _, PORT = server_address
     print(f"Server running on port: {PORT}")
-    threads = []
 
     try:
-        while True:
+        while running:
             client_conn, client_addr = server_socket.accept()
+            connections.append(client_conn)
             print(f'\nI got a connection from{client_addr}')
             print(f'Client Socket: {client_conn}')
-            connections.append(client_conn)
+            print(f'You are connected to: {len(connections)} client(s)')
 
-            client_thread = threading.Thread(target=handle_clients, args=(client_conn,))
+            client_thread = threading.Thread(target=handle_clients,
+                                             args=(client_conn,))
             threads.append(client_thread)
             client_thread.start()
-            print(threading.active_count())
 
-            # for connection in connections:
-            #     response = handle_clients(connection)
-            #     if response == 0:
-            #         print('All clients shutdown.')
+    except OSError:
+            pass
+
     finally:
+        for connection in connections:
+            connection.close()
+        connections[:] = []
+
+        for t in threads:
+            t.join()
+
+        print('\nShutting Down server.')
+        print(f'Connections remaining: {len(connections)}')
         server_socket.close()
-        print('Server shutdown.')
+        print('Server Shutdown!')
 
 start_tcp_server()
