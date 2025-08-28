783b29d666439c5839c1bab90372e06a282d062c
Louie Mendez <lmendezjr024@gmail.com>
2025-08-27 23:01:40 -0400

    Using selectors for event driven I/O multiplexing

--- client.py
+++ client.py
@@ -1,55 +1,42 @@
-import asyncio
-import threading
 import socket
 import msgpack
+import selectors
+import sys
+import io
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 8000  # The port used by the server
-threads = []
-
-def add_threads(thread):
-    threads.append(thread)
-
-def start_threads():
-    for thread in threads:
-        thread.start()
+selector = selectors.DefaultSelector()
 
 def write(client):
-    while True:
-        msg_to_clients = input('')
-        client.sendall(msgpack.packb(msg_to_clients))
+    msg_to_clients = input('')
+    client.sendall(msgpack.packb(msg_to_clients))
 
 def read(client):
-    while True:
-        data = client.recv(1024)
-        if not data:
-            global running
-            running = False
-            print(f'Data {data}. Server closed the connection.\nShutting Down!')
-        else:
-            response = msgpack.unpackb(data)
-            print(f'\nFrom Chat: {response}')
+    data = client.recv(1024)
+    if not data:
+        print(f'Data {data}. Server closed the connection.\nShutting Down!')
+    else:
+        response = msgpack.unpackb(data)
+        print(f'From Chat: {response}')
 
 def run_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
+    selector.register(client, selectors.EVENT_READ)
+    selector.register(sys.stdin, selectors.EVENT_READ)
     try:
-        write_thread = threading.Thread(target=write, args=(client,))
-        read_thread = threading.Thread(target=read, args=(client,))
-        threads.append(read_thread)
-        threads.append(write_thread)
-        start_threads()
+        while True:
+            events = selector.select()
+            for event, _ in events:
+                if isinstance(event.fileobj, io.TextIOWrapper):
+                    write(client)
+                else:
+                    read(client)
     except KeyboardInterrupt:
-        for t in threads:
-            t.join()
-
         client.shutdown(socket.SHUT_RDWR)
         client.close()
     finally:
-
-        for t in threads:
-            t.join()
-
         client.shutdown(socket.SHUT_RDWR)
         client.close()
         print('Client side connection Closed!')
