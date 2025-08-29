2910699c57fe4dd23153d94cc1d9745a26420530
Louie Mendez <lmendezjr024@gmail.com>
2025-08-28 20:48:28 -0400

    using a running flag to control the main event loop; read function returns boolean status indicators to communicate connection state; centralized disconnect function

--- client.py
+++ client.py
@@ -6,7 +6,9 @@ import io
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 8000  # The port used by the server
+running = True
 selector = selectors.DefaultSelector()
+
 
 def write(client):
     msg_to_clients = input('')
@@ -16,29 +18,45 @@ def read(client):
     data = client.recv(1024)
     if not data:
         print(f'Data {data}. Server closed the connection.\nShutting Down!')
+        return False
     else:
         response = msgpack.unpackb(data)
         print(f'From Chat: {response}')
+        return True
+
+def disconnect(client):
+    client.close()
+    print('Client side connection Closed!')
 
 def run_client():
+    global running
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
     selector.register(client, selectors.EVENT_READ)
     selector.register(sys.stdin, selectors.EVENT_READ)
     try:
-        while True:
+        while running:
+
             events = selector.select()
+
             for event, _ in events:
+
+                print(f'event: {event}')
                 if isinstance(event.fileobj, io.TextIOWrapper):
+
+                    print(f'event: {event}\nFileObject: {event.fileobj}')
                     write(client)
                 else:
-                    read(client)
+                    if not read(client):
+                        running = False
+                        break
+                    else:
+                        read(client)
+
     except KeyboardInterrupt:
-        client.shutdown(socket.SHUT_RDWR)
-        client.close()
+        client.sendall(msgpack.packb('quit'))
+        print('sent quit to server')
     finally:
-        client.shutdown(socket.SHUT_RDWR)
-        client.close()
-        print('Client side connection Closed!')
+        disconnect(client)
 
 run_client()
