a0794e4e32d3a89b946364449edaec4e0aeaf2aa
Louie Mendez <lmendezjr024@gmail.com>
2025-09-07 21:48:34 -0400

    small example using zmq publish/subscriber pattern

--- client.py
+++ client.py
@@ -1,61 +1,85 @@
-import socket
-import msgpack
-import selectors
-import sys
-import io
+import zmq, zmq.asyncio
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 8000  # The port used by the server
-running = True
-selector = selectors.DefaultSelector()
+context = zmq.Context()
 
-def write(client):
-    msg_to_clients = input('')
-    client.sendall(msgpack.packb(msg_to_clients))
+#  Socket to talk to server
+print("Connecting to hello world server...")
+socket = context.socket(zmq.SUB)
+socket.connect("tcp://localhost:5556")
+socket.setsockopt_string(zmq.SUBSCRIBE, '10001')
 
-def read(client):
-    data = client.recv(1024)
-    if not data:
-        print(f'Data {data}. Server closed the connection.\nShutting Down!')
-        return False
-    else:
-        response = msgpack.unpackb(data)
-        print(f'From Chat: {response}')
-        return True
+#  Do 10 requests, waiting each time for a response
+for request in range(100):
+    print(f"Sending request {request} ...")
 
-def disconnect(client):
-    client.close()
-    print('Client side connection Closed!')
+    #  Get the reply.
+    message = socket.recv_string()
+    print(f"Received reply {request} [ {message} ]")
 
-def run_client():
-    global running
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((HOST, PORT))
-    selector.register(client, selectors.EVENT_READ)
-    selector.register(sys.stdin, selectors.EVENT_READ)
-    try:
-        while running:
 
-            events = selector.select()
 
-            for event, _ in events:
 
-                print(f'event: {event}')
-                if isinstance(event.fileobj, io.TextIOWrapper):
-
-                    print(f'event: {event}\nFileObject: {event.fileobj}')
-                    write(client)
-                else:
-                    if not read(client):
-                        running = False
-                        break
-                    else:
-                        read(client)
-
-    except KeyboardInterrupt:
-        client.sendall(msgpack.packb('quit'))
-        print('sent quit to server')
-    finally:
-        disconnect(client)
-
-run_client()
+# import socket
+# import zmq, zmq.asyncio
+# import msgpack
+# import selectors
+# import sys
+# import io
+#
+# HOST = "127.0.0.1"
+# PORT = 8000
+# ctx = zmq.asyncio.Context()
+# running = True
+# selector = selectors.DefaultSelector()
+#
+# def write(client):
+#     msg_to_clients = input('')
+#     client.sendall(msgpack.packb(msg_to_clients))
+#
+# def read(client):
+#     data = client.recv(1024)
+#     if not data:
+#         print(f'Data {data}. Server closed the connection.\nShutting Down!')
+#         return False
+#     else:
+#         response = msgpack.unpackb(data)
+#         print(f'From Chat: {response}')
+#         return True
+#
+# def disconnect(client):
+#     client.close()
+#     print('Client side connection Closed!')
+#
+# def run_client():
+#     global running
+#     # client = ctx.socket(zmq.SUB)
+#     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     client.connect((HOST, PORT))
+#     selector.register(client, selectors.EVENT_READ)
+#     selector.register(sys.stdin, selectors.EVENT_READ)
+#     try:
+#         while running:
+#
+#             events = selector.select()
+#
+#             for event, _ in events:
+#
+#                 print(f'event: {event}')
+#                 if isinstance(event.fileobj, io.TextIOWrapper):
+#
+#                     print(f'event: {event}\nFileObject: {event.fileobj}')
+#                     write(client)
+#                 else:
+#                     if not read(client):
+#                         running = False
+#                         break
+#                     else:
+#                         read(client)
+#
+#     except KeyboardInterrupt:
+#         client.sendall(msgpack.packb('quit'))
+#         print('sent quit to server')
+#     finally:
+#         disconnect(client)
+#
+# run_client()
--- server.py
+++ server.py
@@ -1,150 +1,157 @@
-import asyncio, socket, msgpack, logging, signal
-from rich import print
-from asyncio import AbstractEventLoop, create_task, wait_for
-
-connections = {
-    'All': [],
-    'Team_even': {
-        'All': [],
-        'Squads': {
-            'squad_1': [],
-        },
-    },
-    'Team_odd': {
-        'All': [],
-        'Squads': {
-            'squad_1': []
-        },
-    },
-}
-
-all_clients = connections['All']
-team_even = connections['Team_even']
-team_odd = connections['Team_odd']
-
-# class CustomSocket(socket.socket):
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.channel = None
-
-def group_all_clients(client):
-    all_clients.append(client)
-    add_client_to_team(all_clients, client)
-
-def add_client_to_team(all_clients, client):
-    if len(all_clients) % 2 == 0:
-        team_even['All'].append(client)
-        # add_client_to_squad(team_even, client)
-    else:
-        team_odd['All'].append(client)
-        # add_client_to_squad(team_odd, client)
-
-def create_squad_on_current_team(team, id):
-    team['Squads'][f'squad_{id+1}'] = []
-
-# A squad is a group of no more than 2 connections
-def add_client_to_squad(team, client):
-    all_squads = team['Squads']
-    id = len(all_squads)
-    current_squad = all_squads[f'squad_{id}']
-
-    if len(current_squad) < 2:
-        current_squad.append(client)
-    else:
-        create_squad_on_current_team(team, id)
-        add_client_to_squad(team, client)
-
-    # print(f'How many squads in current team {len(all_squads)}\n')
-    # print(f'Team Even total: {len(team_even['Team'])}')
-    # print(f'Team Odd total: {len(team_odd['Team'])}\n')
-    # print(f'All Clients total: {len(connections['All'])} : '
-    #         f'{len(connections['All']) == len(team_even['Team']) +
-    #         len(team_odd['Team'])}')
-
-tasks = []
-
-# def route_message_to_channel(message, client):
-#     match message:
-#         case 'team':
-#             if client in team_even['All']:
-#                 return team_even['All']
-#             else:
-#                 return team_odd['All']
-#         case 'squad':
-#             # if client in team_even
-#             return
+# import asyncio, zmq, zmq.asyncio, socket, msgpack, logging, signal
+# from rich import print
+# from asyncio import AbstractEventLoop, create_task, wait_for
 
 
-async def echo(connection: socket.socket,  loop: AbstractEventLoop) -> None:
-    try:
-        while data := await loop.sock_recv(connection, 1024):
-            print(f'Data coming in: {data}')
-            message = msgpack.unpackb(data)
-            if message == 'boom':
-                raise Exception('Unexpected Network Error')
-            disconnected = await handle_client_disconnects(connection, message)
-            if disconnected:
-                break
-            # clients = route_message_to_correct_clients
-            # print(f'clients {clients}')
+import zmq
+import random
+import sys
+import time
 
+port = "5556"
+if len(sys.argv) > 1:
+    port =  sys.argv[1]
+    int(port)
 
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://*:%s" % port)
 
-            print(type(all_clients))
+while True:
+    topic = random.randrange(9999,10005)
+    messagedata = random.randrange(1,215) - 80
+    print ("%d %d" % (topic, messagedata))
+    socket.send_string("%d %d" % (topic, messagedata))
+    time.sleep(1)
 
-            clients = [client for client_obj in all_clients
-                                for client in list(client_obj.keys())
-                                    if not client == connection]
-
-
-            for client in clients:
-                print(f'WHO IS SENDING: {connection}')
-                await loop.sock_sendall(client, data)
-                print(f'WHO JUST RECEIVED: {client}')
-
-            # for client_obj in all_clients:
-            #     print(client_obj)
-            #     for client in list(client_obj.keys()):
-            #         if not client == connection:
-            #             print(f'WHO IS SENDING: {connection}')
-            #             await loop.sock_sendall(client, data)
-            #             print(f'WHO JUST RECEIVED: {client}')
-
-
-            # for client in all_clients:
-            #     if not client == connection:
-            #         print(f'WHO IS SENDING: {connection}')
-            #         await loop.sock_sendall(client, data)
-            #         print(f'WHO JUST RECEIVED: {client}')
-
-    except Exception as ex:
-        logging.exception(ex)
-    finally:
-        connection.close()
-        if connection in connections:
-            connections['All'].remove(connection)
-
-async def accept_connections(server_socket: socket.socket, loop: AbstractEventLoop):
-    while True:
-        c_conn, c_address = await loop.sock_accept(server_socket)
-        c_conn.setblocking(False)
-        print(f'\nConnection accepted:\n{c_conn} from {c_address}\n')
-        group_all_clients({c_conn: {'channel': 'all'}})
-
-        task = create_task(echo(c_conn, loop))
-        tasks.append(task)
-
-async def handle_client_disconnects(client, message):
-    if message == 'q' or message == 'quit':
-        print('\nClient wants to disconnect.')
-        print('About to close client connection.')
-        client.close()
-        if client in connections:
-            connections['All'].remove(client)
-        print('\nClient Disconnected')
-        print(f'You are connected to {len(connections["All"])} client(s)')
-        return True
-    return False
+# connections = {
+#     'All': [],
+#     'Team_even': {
+#         'All': [],
+#         'Squads': {
+#             'squad_1': [],
+#         },
+#     },
+#     'Team_odd': {
+#         'All': [],
+#         'Squads': {
+#             'squad_1': []
+#         },
+#     },
+# }
+#
+# all_clients = connections['All']
+# team_even = connections['Team_even']
+# team_odd = connections['Team_odd']
+#
+# def group_all_clients(client):
+#     all_clients.append(client)
+#     add_client_to_team(all_clients, client)
+#
+# def add_client_to_team(all_clients, client):
+#     if len(all_clients) % 2 == 0:
+#         team_even['All'].append(client)
+#         # add_client_to_squad(team_even, client)
+#     else:
+#         team_odd['All'].append(client)
+#         # add_client_to_squad(team_odd, client)
+#
+# def create_squad_on_current_team(team, id):
+#     team['Squads'][f'squad_{id+1}'] = []
+#
+# # A squad is a group of no more than 2 connections
+# def add_client_to_squad(team, client):
+#     all_squads = team['Squads']
+#     id = len(all_squads)
+#     current_squad = all_squads[f'squad_{id}']
+#
+#     if len(current_squad) < 2:
+#         current_squad.append(client)
+#     else:
+#         create_squad_on_current_team(team, id)
+#         add_client_to_squad(team, client)
+#
+#     # print(f'How many squads in current team {len(all_squads)}\n')
+#     # print(f'Team Even total: {len(team_even['Team'])}')
+#     # print(f'Team Odd total: {len(team_odd['Team'])}\n')
+#     # print(f'All Clients total: {len(connections['All'])} : '
+#     #         f'{len(connections['All']) == len(team_even['Team']) +
+#     #         len(team_odd['Team'])}')
+#
+# tasks = []
+#
+# async def echo(connection: socket.socket,  loop: AbstractEventLoop) -> None:
+#     try:
+#         while data := await loop.sock_recv(connection, 1024):
+#             print(f'Data coming in: {data}')
+#             message = msgpack.unpackb(data)
+#             if message == 'boom':
+#                 raise Exception('Unexpected Network Error')
+#             disconnected = await handle_client_disconnects(connection, message)
+#             if disconnected:
+#                 break
+#             # clients = route_message_to_correct_clients
+#             # print(f'clients {clients}')
+#
+#
+#
+#             clients = [client for client_obj in all_clients
+#                                 for client in list(client_obj.keys())
+#                                     if not client == connection]
+#
+#
+#             for client in clients:
+#                 print(f'WHO IS SENDING: {connection}')
+#                 await loop.sock_sendall(client, data)
+#                 print(f'WHO JUST RECEIVED: {client}')
+#
+#             # for client_obj in all_clients:
+#             #     print(client_obj)
+#             #     for client in list(client_obj.keys()):
+#             #         if not client == connection:
+#             #             print(f'WHO IS SENDING: {connection}')
+#             #             await loop.sock_sendall(client, data)
+#             #             print(f'WHO JUST RECEIVED: {client}')
+#
+#
+#             # for client in all_clients:
+#             #     if not client == connection:
+#             #         print(f'WHO IS SENDING: {connection}')
+#             #         await loop.sock_sendall(client, data)
+#             #         print(f'WHO JUST RECEIVED: {client}')
+#
+#     except Exception as ex:
+#         logging.exception(ex)
+#     finally:
+#         connection.close()
+#         if connection in connections:
+#             connections['All'].remove(connection)
+#
+# async def accept_connections(server_socket, loop: AbstractEventLoop):
+#     while True:
+#         print('before await')
+#         connection = server_socket.poll()
+#         print('after await')
+#         print(connection)
+#         # c_conn, c_address = await loop.sock_accept(server_socket)
+#         # c_conn.setblocking(False)
+#         # print(f'\nConnection accepted:\n{c_conn} from {c_address}\n')
+#         # group_all_clients({c_conn: {'channel': 'all'}})
+#         #
+#         # task = create_task(echo(c_conn, loop))
+#         # tasks.append(task)
+#
+# async def handle_client_disconnects(client, message):
+#     if message == 'q' or message == 'quit':
+#         print('\nClient wants to disconnect.')
+#         print('About to close client connection.')
+#         client.close()
+#         if client in connections:
+#             connections['All'].remove(client)
+#         print('\nClient Disconnected')
+#         print(f'You are connected to {len(connections["All"])} client(s)')
+#         return True
+#     return False
 
 class GracefulExit(SystemExit):
     pass
@@ -159,22 +166,24 @@ async def close_tasks(tasks: list[asyncio.Task]):
             await task
         except asyncio.exceptions.TimeoutError:
             # Expect a timeout error here
-            print('Did I hit the Timeout Error--!')
+            ('Did I hit the Timeout Error--!')
             pass
 
 async def start_tcp_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.setblocking(False)
+    server_socket = ctx.socket(zmq.PUB)
+    # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # server_socket.setblocking(False)
+    server_socket.bind('tcp://127.0.0.1:8000')
 
-    server_address = ('127.0.0.1', 8000)
-    server_socket.bind(server_address)
-    server_socket.listen(5)
-    _, PORT = server_address
-    print(f"Server running on port: {PORT}")
+    # server_address = ('127.0.0.1', 8000)
+    # server_socket.bind(server_address)
+    # server_socket.listen(5)
+    # _, PORT = server_address
+    # print(f"Server running on port: {PORT}")
 
-    for signame in {'SIGINT', 'SIGTERM'}:
-        loop.add_signal_handler(getattr(signal, signame), shutdown)
+    # for signame in {'SIGINT', 'SIGTERM'}:
+    #     loop.add_signal_handler(getattr(signal, signame), shutdown)
     await accept_connections(server_socket, asyncio.get_event_loop())
 
 loop = asyncio.new_event_loop()
