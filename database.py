5758feb231f1316633d3584dc24d7ee1b9778a93
Louie Mendez <lmendezjr024@gmail.com>
2025-10-31 21:18:29 -0400

    Remove logic from connection file

--- database.py
+++ database.py
@@ -3,29 +3,15 @@ import mysql.connector
 cnx = None
 cursor = None
 
-try:
-    cnx = mysql.connector.connect(
-        user="root", password="", host="127.0.0.1", database="menagerie"
-    )
-    if cnx.is_connected():
-        print("connnected to mysql database")
 
-    cursor = cnx.cursor()
-    query = "SELECT * from pet"
-    cursor.execute(query)
-    row = cursor.fetchone()
+def connect():
+    try:
+        cnx = mysql.connector.connect(
+            user="root", password="esdf24", host="127.0.0.1", database="chat_data"
+        )
+        if cnx.is_connected():
+            print("connnected to mysql database")
+            return cnx
 
-    while row is not None:
-        print(row)
-        row = cursor.fetchone()
-
-except mysql.connector.Error as e:
-    print(e)
-
-finally:
-    if cursor:
-        cursor.close()
-        print("cursor closed")
-    if cnx:
-        cnx.close()
-        print("database connection closed")
+    except mysql.connector.Error as e:
+        print(e)
