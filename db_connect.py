f13a10551a858397066e9642a41206d682b58872
Louie Mendez <lmendezjr024@gmail.com>
2025-11-04 21:05:11 -0500

    Rename file for more accurate description

---
+++ db_connect.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+
+def db_connection():
+    try:
+        cnx = mysql.connector.connect(
+            user="root", password="esdf24", host="127.0.0.1", database="chat_data"
+        )
+        if cnx.is_connected():
+            print("connnected to mysql database")
+            return cnx
+
+    except mysql.connector.Error as e:
+        print(e)
